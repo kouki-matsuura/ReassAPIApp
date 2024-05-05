b4c636fcf7054658a974dc73b9cc2a997657e3b6
松浦　昂生 <kouki19991222@icloud.com>
2024-05-05 15:43:24 +0900

    prettierとeslintの設定

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,76 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react/jsx-runtime",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.ts"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "unused-imports"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "variable",
+        format: ["camelCase"],
+      },
+      {
+        selector: "function",
+        format: ["camelCase", "PascalCase"],
+      },
+      {
+        selector: "parameter",
+        format: ["camelCase"],
+      },
+      {
+        selector: "class",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "method",
+        format: ["camelCase"],
+      },
+      {
+        selector: "property",
+        format: ["camelCase"],
+      },
+      {
+        selector: "interface",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "typeAlias",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "typeParameter",
+        format: ["camelCase"],
+      },
+      {
+        selector: "enum",
+        format: ["PascalCase"],
+      },
+      {
+        selector: "enumMember",
+        format: ["UPPER_CASE"],
+      },
+    ],
   },
-}
+};
